bfad58c47b5fb9e16c2554704b2afa3a2eedfeb8
bougueil <rmariana@gmail.com>
2022-12-05 11:14:12 +0100

    os_mon config

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,8 @@
-use Mix.Config
+import Config
+
+# https://spapas.github.io/2020/05/15/elixir-osmon-alerts/
+
+config :os_mon,
+  memsup_system_only: true,
+  start_disk_sup: false,
+  start_cpu_sup: false
