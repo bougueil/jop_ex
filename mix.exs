347ac431195f1729d5cab0f2ab0dfcd6fcbaa329
bougueil <rmariana@gmail.com>
2022-12-05 11:08:56 +0100

    credo

--- mix.exs
+++ mix.exs
@@ -5,8 +5,9 @@ defmodule Jop.Mixfile do
     [
       app: :jop,
       version: "0.1.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env() == :prod
+      elixir: "~> 1.14.1",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
     ]
   end
 
@@ -15,4 +16,14 @@ defmodule Jop.Mixfile do
       extra_applications: []
     ]
   end
+
+    # Run "mix help deps" to learn about dependencies.
+    defp deps do
+      [
+        {:ex_doc, "~> 0.29.1", only: [:docs, :test], runtime: false},
+        {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
+        {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      ]
+    end
+
 end
