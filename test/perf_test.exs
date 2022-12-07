bd24d22f14bcd181f1ab4368341b2406742ff7ac
bougueil <rmariana@gmail.com>
2022-12-07 23:47:39 +0100

    small refactor

--- test/perf_test.exs
+++ test/perf_test.exs
@@ -6,8 +6,14 @@ defmodule PerfTest do
     joplog = JopLog.init(@jop_log)
 
     n = 1000_000
-    bench_fn = fn -> for i <- 1..n, do: JopLog.log(joplog, <<i::size(40)>>, {<<"data.", <<i:: size(40)>>::binary>>}) end
-    {tlog, _} = :timer.tc(bench_fn)
+
+    {tlog, _} =
+      fn ->
+        for i <- 1..n,
+            do: JopLog.log(joplog, <<i::size(40)>>, {<<"data.", <<i::size(40)>>::binary>>})
+      end
+      |> :timer.tc()
+
     througput = div(n * 1_000_000, tlog)
     IO.puts("througput #{througput} logs/s.")
     assert Enum.count(joplog) == n
